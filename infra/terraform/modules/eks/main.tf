d37eb6d2f7cb1c674a58c77c0a8c6fbe88cfc068
mhieu <mhieudzvcl@gmail.com>
2026-05-23 23:19:00 +0700

    Deployed app on AWS EKS

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "this" {
   vpc_config {
     subnet_ids              = var.private_subnet_ids
     endpoint_private_access = true
-    endpoint_public_access  = false
+    endpoint_public_access  = true
   }
 
   encryption_config {
@@ -15,6 +15,11 @@ resource "aws_eks_cluster" "this" {
     }
     resources = ["secrets"]
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_vpc_resource_controller,
+  ]
 }
 
 resource "aws_kms_key" "eks" {
@@ -32,4 +37,68 @@ resource "aws_iam_role" "eks_cluster_role" {
       Principal = { Service = "eks.amazonaws.com" }
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.project_name}-node-group-v6"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.private_subnet_ids
+  instance_types  = ["t3.small"]
+  ami_type        = "BOTTLEROCKET_x86_64"
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 4
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_read_only,
+    aws_iam_role_policy_attachment.eks_ssm_policy,
+  ]
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.project_name}-eks-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ssm_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.eks_node_role.name
 }
--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -5,16 +5,50 @@ resource "aws_vpc" "this" {
   tags = { Name = "${var.project_name}-vpc" }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags   = { Name = "${var.project_name}-igw" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "${var.project_name}-public-rt" }
+}
+
 resource "aws_subnet" "private_a" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1a"
-  tags = { Name = "${var.project_name}-private-a" }
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project_name}-public-a"
+    "kubernetes.io/cluster/${var.project_name}-eks" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_subnet" "private_b" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-1b"
-  tags = { Name = "${var.project_name}-private-b" }
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project_name}-public-b"
+    "kubernetes.io/cluster/${var.project_name}-eks" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.public.id
 }
